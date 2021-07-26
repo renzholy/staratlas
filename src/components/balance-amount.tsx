b963885fe4fd5ca5f3d3463d13f400537a4b7d1b
Found Pan Tiger <renzholy@gmail.com>
2021-07-26 22:19:12 +0800

    Update balance-amount.tsx

--- src/components/balance-amount.tsx
+++ src/components/balance-amount.tsx
@@ -3,12 +3,12 @@ import { Text } from '@chakra-ui/react'
 import { useScalingFactor } from '../hooks/use-contract'
 import { formatNumberPrecision } from '../utils/formatter'
 
-export default function BalanceAmount(props: { token: string; value: bigint | number }) {
+export default function BalanceAmount(props: { token: string; value: BigInt | number }) {
   const { data } = useScalingFactor(props.token)
 
   return (
     <Text color="gray.500">
-      {data ? formatNumberPrecision(BigInt(props.value) / BigInt(data[0])) : '-'}
+      {data ? formatNumberPrecision(BigInt(props.value.toString()) / BigInt(data[0])) : '-'}
     </Text>
   )
 }
