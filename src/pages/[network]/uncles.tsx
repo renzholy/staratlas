fd3a8bd95903775f37d39b394869d41e3ae9ba23
Found Pan Tiger <renzholy@gmail.com>
2021-07-28 17:09:33 +0800

    add uncles page

--- src/pages/[network]/uncles.tsx
+++ src/pages/[network]/uncles.tsx
@@ -9,20 +9,20 @@ import {
   useColorMode,
 } from '@chakra-ui/react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
-import BlockListItem from 'components/block-list-item'
 import ListItemPlaceholder from 'components/list-item-placeholder'
 import { CardWithHeader } from 'layouts/card-with-header'
-import { useBlocksByHeight } from 'hooks/use-api'
+import UncleListItem from 'components/uncle-list-item'
+import { useUnclesByHeight } from 'hooks/use-api'
 import useJsonRpc from 'hooks/use-json-rpc'
 
 const SIZE = 20
 
-export default function Blocks() {
+export default function Uncles() {
   const { data: info } = useJsonRpc('chain.info', [], { revalidateOnFocus: false })
-  const [blockHeight, setBlockHeight] = useState<bigint | undefined>()
-  const { data: blocks } = useBlocksByHeight(blockHeight, { revalidateOnFocus: false })
+  const [uncleHeight, setUncleHeight] = useState<bigint | undefined>()
+  const { data: uncles } = useUnclesByHeight(uncleHeight, { revalidateOnFocus: false })
   useEffect(() => {
-    setBlockHeight(info ? BigInt(info.head.number) : undefined)
+    setUncleHeight(info ? BigInt(info.head.number) : undefined)
   }, [info])
   const { colorMode } = useColorMode()
 
@@ -34,7 +34,7 @@ export default function Blocks() {
     >
       <GridItem colSpan={1}>
         <CardWithHeader
-          title="Blocks"
+          title="Uncles"
           subtitle={
             <>
               <ButtonGroup
@@ -45,33 +45,33 @@ export default function Blocks() {
                 mr={-4}
               >
                 <IconButton
-                  aria-label="prev block"
+                  aria-label="prev uncle"
                   icon={<ChevronLeftIcon />}
                   mr="-px"
                   bg={colorMode === 'light' ? 'white' : undefined}
                   onClick={() => {
-                    setBlockHeight((old) => (old ? old + BigInt(SIZE) : old))
+                    setUncleHeight((old) => (old ? old + BigInt(SIZE) : old))
                   }}
-                  disabled={!info || !blockHeight || blockHeight >= BigInt(info.head.number)}
+                  disabled={!info || !uncleHeight || uncleHeight >= BigInt(info.head.number)}
                 />
                 <IconButton
-                  aria-label="next block"
+                  aria-label="next uncle"
                   icon={<ChevronRightIcon />}
                   bg={colorMode === 'light' ? 'white' : undefined}
                   onClick={() => {
-                    setBlockHeight((old) => (old ? old - BigInt(SIZE) : old))
+                    setUncleHeight((old) => (old ? old - BigInt(SIZE) : old))
                   }}
-                  disabled={!blockHeight || blockHeight - BigInt(SIZE) <= BigInt(0)}
+                  disabled={!uncleHeight || uncleHeight - BigInt(SIZE) <= BigInt(0)}
                 />
               </ButtonGroup>
             </>
           }
         >
-          {blocks?.length ? (
-            blocks.map((block, index) => (
-              <Fragment key={block._id}>
+          {uncles?.length ? (
+            uncles.map((uncle, index) => (
+              <Fragment key={uncle._id}>
                 {index === 0 ? null : <Divider />}
-                <BlockListItem block={block._id} />
+                <UncleListItem uncle={uncle._id} />
               </Fragment>
             ))
           ) : (
