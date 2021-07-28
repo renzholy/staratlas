42deabde68acbe01141ad2f804192d9a2017be17
Found Pan Tiger <renzholy@gmail.com>
2021-07-28 22:02:05 +0800

    Update blocks.tsx

--- src/pages/[network]/blocks.tsx
+++ src/pages/[network]/blocks.tsx
@@ -1,21 +1,9 @@
 import { Fragment, useState, useEffect } from 'react'
-import {
-  Divider,
-  Grid,
-  GridItem,
-  Spinner,
-  ButtonGroup,
-  IconButton,
-  useColorMode,
-} from '@chakra-ui/react'
-import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons'
+import { Divider, Center, Box, Spinner } from '@chakra-ui/react'
 import BlockListItem from 'components/block-list-item'
-import ListItemPlaceholder from 'components/list-item-placeholder'
-import { CardWithHeader } from 'layouts/card-with-header'
 import { useBlocksByHeight } from 'hooks/use-api'
 import useJsonRpc from 'hooks/use-json-rpc'
-
-const SIZE = 10
+import { CardWithHeader } from 'layouts/card-with-header'
 
 export default function Blocks() {
   const { data: info } = useJsonRpc('chain.info', [], { revalidateOnFocus: false })
@@ -24,63 +12,23 @@ export default function Blocks() {
   useEffect(() => {
     setBlockHeight(info ? BigInt(info.head.number) : undefined)
   }, [info])
-  const { colorMode } = useColorMode()
 
   return (
-    <Grid
-      templateColumns={{ base: 'minmax(0, 1fr)', xl: 'minmax(0, 1fr) minmax(0, 1fr)' }}
-      gap={6}
-      padding={6}
-    >
-      <GridItem colSpan={1}>
-        <CardWithHeader
-          title="Blocks"
-          subtitle={
-            <>
-              <ButtonGroup
-                size="sm"
-                isAttached={true}
-                variant={colorMode === 'light' ? 'outline' : undefined}
-                spacing={0}
-                mr={-4}
-              >
-                <IconButton
-                  aria-label="prev block"
-                  icon={<ChevronLeftIcon />}
-                  mr="-px"
-                  bg={colorMode === 'light' ? 'white' : undefined}
-                  onClick={() => {
-                    setBlockHeight((old) => (old ? old + BigInt(SIZE) : old))
-                  }}
-                  disabled={!info || !blockHeight || blockHeight >= BigInt(info.head.number)}
-                />
-                <IconButton
-                  aria-label="next block"
-                  icon={<ChevronRightIcon />}
-                  bg={colorMode === 'light' ? 'white' : undefined}
-                  onClick={() => {
-                    setBlockHeight((old) => (old ? old - BigInt(SIZE) : old))
-                  }}
-                  disabled={!blockHeight || blockHeight - BigInt(SIZE) <= BigInt(0)}
-                />
-              </ButtonGroup>
-            </>
-          }
-        >
-          {blocks?.length ? (
-            blocks.map((block, index) => (
+    <Center gap={6} padding={6} width="100%">
+      <Box maxWidth="xl">
+        {blocks?.length ? (
+          <CardWithHeader>
+            {blocks.map((block, index) => (
               <Fragment key={block._id}>
                 {index === 0 ? null : <Divider />}
                 <BlockListItem block={block._id} />
               </Fragment>
-            ))
-          ) : (
-            <ListItemPlaceholder height={SIZE * 68 - 1}>
-              <Spinner />
-            </ListItemPlaceholder>
-          )}
-        </CardWithHeader>
-      </GridItem>
-    </Grid>
+            ))}
+          </CardWithHeader>
+        ) : (
+          <Spinner />
+        )}
+      </Box>
+    </Center>
   )
 }
