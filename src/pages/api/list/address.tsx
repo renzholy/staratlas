5081f6df9887d1ac5be38fdeeecaa16600811886
Found Pan Tiger <renzholy@gmail.com>
2021-07-29 21:59:58 +0800

    Update address.tsx

--- src/pages/api/list/address.tsx
+++ src/pages/api/list/address.tsx
@@ -1,4 +1,4 @@
-import { Binary } from 'bson'
+import { Binary, Decimal128 } from 'bson'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { collections } from 'utils/database/mongo'
 import { Network } from 'utils/types'
@@ -6,24 +6,38 @@ import { mapper, Type } from 'utils/api'
 import { maintenance } from 'utils/database/maintenance'
 import { arrayify } from 'utils/encoding'
 
-async function list(network: Network, type: Type, address: string) {
+const LIMIT = 10
+
+async function list(network: Network, type: Type, address: string, height?: string) {
   switch (type) {
     case 'block': {
       return collections[network].blocks
-        .find({ author: new Binary(arrayify(address)) })
+        .find({
+          author: new Binary(arrayify(address)),
+          ...(height ? { height: { $lte: new Decimal128(height) } } : {}),
+        })
         .sort({ height: -1 })
+        .limit(LIMIT)
         .toArray()
     }
     case 'transaction': {
       return collections[network].transactions
-        .find({ sender: new Binary(arrayify(address)) })
+        .find({
+          sender: new Binary(arrayify(address)),
+          ...(height ? { height: { $lte: new Decimal128(height) } } : {}),
+        })
         .sort({ height: -1 })
+        .limit(LIMIT)
         .toArray()
     }
     case 'uncle': {
       return collections[network].uncles
-        .find({ author: new Binary(arrayify(address)) })
+        .find({
+          author: new Binary(arrayify(address)),
+          ...(height ? { height: { $lte: new Decimal128(height) } } : {}),
+        })
         .sort({ height: -1 })
+        .limit(LIMIT)
         .toArray()
     }
     default: {
@@ -36,7 +50,12 @@ export default async function ListByAddress(
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> {
-  const { type, network, address } = req.query as { network: Network; type: Type; address: string }
+  const { type, network, address } = req.query as {
+    network: Network
+    type: Type
+    address: string
+    height?: string
+  }
 
   const data = await list(network, type, address)
 
