60701721450b69cd9515f9c253999b20681c358a
Found Pan Tiger <renzholy@gmail.com>
2021-07-31 11:12:26 +0800

    Update use-json-rpc.ts

--- src/hooks/use-json-rpc.ts
+++ src/hooks/use-json-rpc.ts
@@ -1,7 +1,5 @@
 import useSWR, { SWRConfiguration } from 'swr'
 import type { Static } from '@sinclair/typebox'
-import { useMemo } from 'react'
-
 import useNetwork from 'hooks/use-network'
 import { API, jsonRpc } from 'utils/json-rpc'
 
@@ -11,12 +9,8 @@ export default function useJsonRpc<T extends keyof typeof API>(
   config?: SWRConfiguration,
 ) {
   const network = useNetwork()
-  const key = useMemo(
-    () => (method && params ? [network, 'rpc', method, JSON.stringify(params)] : null),
-    [method, network, params],
-  )
   return useSWR<Static<typeof API[T]['result']>>(
-    key,
+    method && params ? [network, 'rpc', method, JSON.stringify(params)] : null,
     () => jsonRpc(network, method!, params!),
     config,
   )
