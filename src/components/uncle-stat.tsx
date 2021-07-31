deb9ca3eb2ccca0399b4227f3164046b7311c935
Found Pan Tiger <renzholy@gmail.com>
2021-07-31 13:56:00 +0800

    UncleStat

---
+++ src/components/uncle-stat.tsx
@@ -0,0 +1,62 @@
+import { Grid, GridItem, Stat, StatLabel, Skeleton, StatNumber } from '@chakra-ui/react'
+import { BlockHeader } from 'utils/json-rpc/chain'
+import { formatNumber, formatTime } from 'utils/formatter'
+import { Static } from '@sinclair/typebox'
+
+export default function UncleStat(props: { uncle?: Static<typeof BlockHeader> }) {
+  const { uncle } = props
+
+  return (
+    <>
+      <GridItem colSpan={1}>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+          gap={6}
+          paddingX={{ base: 0, md: 6 }}
+          whiteSpace="nowrap"
+        >
+          <GridItem colSpan={1}>
+            <Stat>
+              <StatLabel>Height</StatLabel>
+              <Skeleton isLoaded={!!uncle}>
+                <StatNumber>#{uncle?.number}</StatNumber>
+              </Skeleton>
+            </Stat>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <Stat>
+              <StatLabel>Difficulty</StatLabel>
+              <Skeleton isLoaded={!!uncle}>
+                <StatNumber>{uncle ? formatNumber(BigInt(uncle.difficulty)) : '-'}</StatNumber>
+              </Skeleton>
+            </Stat>
+          </GridItem>
+        </Grid>
+      </GridItem>
+      <GridItem colSpan={1}>
+        <Grid
+          templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(2, 1fr)' }}
+          gap={6}
+          paddingX={{ base: 0, md: 6 }}
+        >
+          <GridItem colSpan={1}>
+            <Stat>
+              <StatLabel>Nonce</StatLabel>
+              <Skeleton isLoaded={!!uncle}>
+                <StatNumber>{uncle ? formatNumber(BigInt(uncle.nonce)) : '-'}</StatNumber>
+              </Skeleton>
+            </Stat>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <Stat>
+              <StatLabel>Timestamp</StatLabel>
+              <Skeleton isLoaded={!!uncle}>
+                <StatNumber>{uncle ? formatTime(parseInt(uncle.timestamp, 10)) : '-'}</StatNumber>
+              </Skeleton>
+            </Stat>
+          </GridItem>
+        </Grid>
+      </GridItem>
+    </>
+  )
+}
