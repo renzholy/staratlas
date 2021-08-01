9403433c5133da605e372944aeec3425fa6b962e
Found Pan Tiger <renzholy@gmail.com>
2021-08-01 21:32:20 +0800

    Update maintenance.ts

--- src/utils/database/maintenance.ts
+++ src/utils/database/maintenance.ts
@@ -130,10 +130,8 @@ export async function load(network: Network, top: BigInt) {
       if (err instanceof MongoServerError && err.code === 11000) {
         const height = err.message.match(/dup key: \{ height: (\d+) \}/)?.[1]
         if (height) {
-          await Promise.all([
-            collections[network].transactions.deleteMany({ height: new Long(height) }),
-            collections[network].blocks.deleteOne({ height: new Long(height) }),
-          ])
+          await collections[network].transactions.deleteMany({ height: new Long(height) })
+          await collections[network].blocks.deleteOne({ height: new Long(height) })
         }
       }
       throw err
